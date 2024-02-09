a7c7b9b15e1689ac652641424f76dd9504b1eeea
gabrielfbarbosa <gfelipebarbosa@yahoo.com>
2024-02-09 02:11:24 -0300

    F04-001: Refactor product handling and integrate search functionality

--- app/src/main/java/com/nationdev/shopapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/nationdev/shopapp/ui/screens/HomeScreen.kt
@@ -1,36 +1,88 @@
 package com.nationdev.shopapp.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nationdev.shopapp.model.Product
+import com.nationdev.shopapp.sampledata.sampleSections
 import com.nationdev.shopapp.ui.components.ProductsSection
-import com.nationdev.shopapp.ui.components.sampleProducts
+import com.nationdev.shopapp.ui.theme.ShopAppTheme
 
 @Composable
-fun HomeScreen() {
-    Column (
-        Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ){
-        Spacer(Modifier)
-        ProductsSection("Promoçoes", sampleProducts)
-        ProductsSection("Crianças", sampleProducts)
-        ProductsSection("Fruits", sampleProducts)
-        Spacer(Modifier)
+fun HomeScreen(
+    sections: Map<String, List<Product>>
+) {
+    Column {
+        var text by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newValue ->
+                text = newValue
+            }, Modifier
+                .padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp
+                )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(35),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "ícone de lupa")
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text("O que você procura?")
+            }
+        )
+
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductsSection(
+                        title = title,
+                        products = products
+                    )
+                }
+            }
+        }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    ShopAppTheme {
+        Surface {
+            HomeScreen(sampleSections)
+        }
+    }
 }
