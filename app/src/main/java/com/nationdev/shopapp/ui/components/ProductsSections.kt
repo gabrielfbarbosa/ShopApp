d5835e9a449aa027ba757140b2724a9317ebaad0
gabrielfbarbosa <gfelipebarbosa@yahoo.com>
2024-02-11 03:09:38 -0300

    Remove duplicate sample products from codebase

--- app/src/main/java/com/nationdev/shopapp/ui/components/ProductsSections.kt
+++ app/src/main/java/com/nationdev/shopapp/ui/components/ProductsSections.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nationdev.shopapp.model.Product
+import com.nationdev.shopapp.sampledata.sampleProducts
 import com.nationdev.shopapp.ui.theme.ShopAppTheme
 import java.math.BigDecimal
 
@@ -57,30 +58,3 @@ fun ProductsSectionPreview() {
         }
     }
 }
-
-
-val sampleProducts = listOf(
-    Product(
-        name = "Davi",
-        price = BigDecimal("39.90"),
-        image = "https://www.otempo.com.br/image/contentid/policy:1.2838645:1679957625/davi-jpg.jpg?f=3x2&w=1224"
-    ),
-
-    Product(
-        name = "Sara",
-        price = BigDecimal("39.90"),
-        image = "https://criticalhits.com.br/wp-content/uploads/2021/08/Kujou-sara.jpg"
-    ),
-
-    Product(
-        name = "Kitsune",
-        price = BigDecimal("12.50"),
-        image = "https://acdn.mitiendanube.com/stores/003/317/647/products/kitsune-4dc29761789be3417217040582094703-1024-1024.png"
-    ),
-
-    Product(
-        name = "Venom",
-        price = BigDecimal("12.50"),
-        image = "https://img.olhardigital.com.br/wp-content/uploads/2023/10/Venom-1.jpg"
-    )
-)
